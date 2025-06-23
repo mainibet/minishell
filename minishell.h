fe265b5d45aa9017bc1c153f601cea2e2e41dfb9
Alicia Betancourt <141577801+mainibet@users.noreply.github.com>
2025-06-23 08:29:02 +0200

    Update minishell.h

--- minishell.h
+++ minishell.h
@@ -26,9 +26,9 @@
 //STDLIB.H FOR: malloc, free, exit, getenv
 //UNISTD.H FOR: read, write, access and others
 //may be here also later: sys/wait, sys/types: size_t, pid_t, strings.h
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/wait.h>
 
 // -----------------------------------------//
