558aa165ddafc1d49c95e0b31f2ba489bc34f423
Alicia Betancourt <141577801+mainibet@users.noreply.github.com>
2025-06-28 14:06:14 +0200

    Feat: macros for max_args and BUFFER_SIZE
    BUFFER_SIZE to read from command line, here_doc or any other fd

--- minishell.h
+++ minishell.h
@@ -18,6 +18,8 @@
 // -----------------------------------------//
 
 //include them here if needed 
+//#define MAX_ARGS 256 //checkif needed
+#define BUFFER_SIZE 1024//to read from command line, from fd or from here_doc
 
 // -----------------------------------------//
 //        STANDARD HEADERS (COMMON)         //
