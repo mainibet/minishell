1965678eca599f185c27c27f66408b03ea2a1b53
Alicia Betancourt <141577801+mainibet@users.noreply.github.com>
2025-06-28 14:07:22 +0200

    Style: included string.h for lexing

--- minishell.h
+++ minishell.h
@@ -32,6 +32,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <sys/wait.h>
+# include <string.h>
 
 // -----------------------------------------//
 //             MODULE HEADERS               //
