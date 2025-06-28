67b40980adddbfd3641274219881ee22da15e150
Alicia Betancourt <141577801+mainibet@users.noreply.github.com>
2025-06-28 14:09:45 +0200

    Undo string.h
    Used in lex for memcpy, not allowed ft

--- minishell.h
+++ minishell.h
@@ -32,7 +32,6 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <sys/wait.h>
-# include <string.h>
 
 // -----------------------------------------//
 //             MODULE HEADERS               //
