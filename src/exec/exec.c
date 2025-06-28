e1a1cc6f41c841c0f2ca5a4a42bbee271a2c179f
Alicia Betancourt Mainieri <albetanc@c1b4c3.42berlin.de>
2025-06-28 18:06:34 +0200

    style: included dir exec and lexer

---
+++ src/exec/exec.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 16:32:13 by albetanc          #+#    #+#             */
+/*   Updated: 2025/06/23 08:33:46 by albetanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+//---------------------------------------//
+//V0:Execute single commands directly,   //
+//   from readline                       //
+//                                       //
+//V1:supports basic parsing              //
+//                                       //
+//V2:support parsing refinement          //
+//                                       //
+//V3:execution with pipes & child pcs    //
+//                                       //
+//V4: support all built-ins              //
+//                                       //
+//V5: support heredoc & redirections     //
+//---------------------------------------//
+
