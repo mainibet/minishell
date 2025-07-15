cfdb482737261ab9663439d12076f91299ee749f
Alicia Betancourt Mainieri <albetanc@c1b4c3.42berlin.de>
2025-07-15 15:48:08 +0200

    included structs and minishell.h

--- include/parser.h
+++ include/parser.h
@@ -6,17 +6,20 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 13:34:47 by albetanc          #+#    #+#             */
-/*   Updated: 2025/07/15 13:54:13 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/07/15 15:45:28 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
-#include "minishell.h"
+# include "minishell.h"
 
 // ---FORWARD DECLARATIONS--- //
-
+typedef struct s_token	t_token;
+typedef struct s_node	t_node;
+enum e_toktype;
+enum e_nodetype;
 
 // -----------------------------------------//
 //                  STRUCTS                 //
@@ -44,6 +47,8 @@ typedef struct s_node
 // -----------------------------------------//
 //                PROTOTYPES                //
 // -----------------------------------------//
+
 t_node	*parse(t_token **token, int min_precedence);
+void	free_node(t_node *node);
 
 #endif
