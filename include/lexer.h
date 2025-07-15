5a7514440b9eda3f014da04968c39b36c25c6958
Alicia Betancourt Mainieri <albetanc@c1b4c3.42berlin.de>
2025-07-15 08:54:47 +0200

    feat: new .h from lexer.c

---
+++ include/lexer.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 08:36:45 by albetanc          #+#    #+#             */
+/*   Updated: 2025/07/15 08:53:15 by albetanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "include/minishell.h"
+
+// -----------------------------------------//
+//                  STRUCTS                 //
+// -----------------------------------------//
+
+typedef struct s_token
+{
+	char			*txt;
+	t_token			*next;
+	enum e_toktype	type;
+	char			delim;
+}	t_token;
+
+
+// -----------------------------------------//
+//                PROTOTYPES                //
+// -----------------------------------------//
+
+t_token			*lex(char *s, char delim);
+void			free_token(t_token *token);
+enum e_toktype	token_type(t_token *token);
+t_token			*extract_token(char *s, size_t size);
