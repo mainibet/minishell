9315f6a980b8676b6a6fd6202d62a43756a72fc8
Your Name <you@example.com>
2025-07-15 14:56:06 +0200

    feat(update_proto.sh): script to add and update the header prototypes from the (compilable) .c files

---
+++ util/update_proto.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+[[ -z "$1" ]] && exit 1
+which cproto > /dev/null || alias cproto="docker run cproto -v $PWD:/cproto"
+tmp=$(mktemp)
+sed '/\/\* prototypes \*\//q' $1 > tmp
+cproto -F"int\tf(a, b)" *.c | \
+	   sed -E 's/^([a-z_]+) /\1\t/' | \
+	   sed 's/^int\t/int\t\t/' | \
+	   grep -v 'main(' >> tmp
+echo '#endif' >> tmp
+cp tmp $1
