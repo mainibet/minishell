585f9663c12c505daa6a6c6ac75fa9f831d16d06
Alicia Betancourt Mainieri <albetanc@c1b4c3.42berlin.de>
2025-07-16 08:30:46 +0200

    feature: simplified exec.h by moving things to minishell.h

--- src/exec/exec.h
+++ src/exec/exec.h
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 11:02:07 by albetanc          #+#    #+#             */
-/*   Updated: 2025/07/15 17:36:23 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/07/16 08:28:27 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,51 +15,12 @@
 
 # include "minishell.h"
 
-// --- FORWARD DECLARATION --- //
-typedef struct s_node t_node;
-
 // -----------------------------------------//
 //                 STRUCTS                  //
 // -----------------------------------------//
 
-typedef enum e_cmdtype
-{
-	EXECUTABLE,
-	BUILTIN,
-}	t_cmdtype;
-
-// --- INITIAL VERSION CMD NODE --- // 
-/**
-*   @brief stores data relevand in cmd_node AST:
-*   single external cmd
-*
-*   @note This struct intend to be part of `u_data' 
-*   union in AST
-*   @note `argv` will be later `t_token *tokens`
-*   @note future version might have io[2] instead of 
-*   @note future veresion might have int fd_other[MAX_OPEN_FDs]
-*   if so they will need to be closed before child 
-*   if not needed fd_in fd_out received
-*/
-typedef struct s_cmd_data
-{
-	char		**argv;
-	char		**env;
-	int			fd_in;
-	int			fd_out;
-	t_cmdtype	cmd_type;
-}	t_cmd_data;
-
-typedef struct s_pipe_data
-{
-	t_node	*left;
-	t_node	*right;
-}	t_pipe_data;
-
-
 /**
  *  @brief stores duplicated fd for tmp redirection
- * 
  *  -Use: to hold tmp compies of STDIN_FILENO/ STDOU_FILENO
 */
 typedef struct s_fd_dup
