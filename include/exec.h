6707481de798b0d65f16d837915f0dc8f09b6789
Alicia Betancourt Mainieri <albetanc@c1b4c3.42berlin.de>
2025-07-21 11:40:43 +0200

    included function execution

--- include/exec.h
+++ include/exec.h
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 11:02:07 by albetanc          #+#    #+#             */
-/*   Updated: 2025/07/16 09:50:35 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/07/21 11:39:55 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ char	*find_path(char *argv);
 int		fork_handle(pid_t *pid, t_node *node, int i_cmd, int nb_cmd);
 int		setup_redir(int fd_in, int fd_out, t_fd_dup *dup);
 int		wait_one_child(pid_t pid, int *status);
-int		execute_cmd(t_node *node);
+void	execution(t_node *cmd);
 
 //child
 void	child_process(t_node *node);
