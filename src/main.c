a9d14fa8659f07e5308e09c21e797f1fdb21b989
Alicia Betancourt Mainieri <albetanc@c1b4c3.42berlin.de>
2025-07-21 11:42:45 +0200

    refactored main to include mvp of token and parsing and pre-execution besides the execution

--- src/main.c
+++ src/main.c
@@ -6,11 +6,11 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 08:42:03 by albetanc          #+#    #+#             */
-/*   Updated: 2025/07/16 09:50:02 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/07/21 11:37:27 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../include/minishell.h"
 
 //-------------------------------//
 //          V0: main             //
@@ -23,12 +23,65 @@
 // includes add_history          //
 //-------------------------------//
 
+t_node	*parse_input(char *line, t_token **token_out)
+{
+	t_token	*token_list;
+	t_token	*parser_tokens;
+	t_node	*root;
+// --- lexer --- //
+	token_list = lex(line, ' ');
+	if (!token_list)
+	{
+		fprintf(stderr, BOLD RED "Lexing failed or empty imput\n" RESET)//test
+		*token_out = NULL;//test
+		return (NULL);
+	}
+	fprintf(stderr, MAGENTA "Tokens generated: " RESET);//test
+	print_tokens(token_list);//test
+	fprintf(stderr, MAGENTA"\n" RESET);//test
+// --- parser ---//
+	parser_tokens = token_list;
+	root = parse(&parser_tokens, 0);//0 is the minimum precedency to begin
+	if (!root)
+	{
+		fprintf(stderr, "Parsing failed\n");
+		free_token(token_list);
+		*token_out = NULL;
+		return (NULL);
+	}
+	*token_out = token_list;
+	return (root);
+}
+
+static void	handle_command(char *line, char **envp)
+{
+	t_token	*token_list;
+	t_node	*root;
+
+	if (!line)//handles NULL line
+		return ;
+	if (!*line)//handles empty line
+	{
+		free(line);
+		return ;
+	}
+	add_history(line);
+	root = parse_input(line, &token_list);
+	free(line);
+	if (!root)//if parsing failed
+		return ;
+	fprintf(stderr, BOLD MAGENTA "AST built. stating pre-execution \n" RESET);//TEST
+	pre_execution(root, envp);
+	execution(root);
+	free_token(token_list);
+    free_node(root);
+    fprintf(stderr, BOLD MAGENTA "Command processed and cleaned up\n" RESET); //TEST
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	char	*line;
 	char	*prompt;
-	t_token	*token_list;
-	t_node	*root;
 
 	(void) argc;//check if needed
 	(void) argv;//check if needed
@@ -37,21 +90,15 @@ int	main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		line = readline(prompt);
-		if (line == NULL)
+		if (!line && isatty(STDIN_FILENO)) //if issaty returns 0 is in an fd
 		{
 			printf(BLUE "exit\n" RESET);
 			break ;
 		}
-		if (*line)
-			add_history(line);
 		printf(BOLD MAGENTA "Command received: %s\n" RESET, line);//test
-		// --- lexing --- //
-        token_list = lex(line, ' ');
-		if (token_list)
-		{
-			free(line);
-			continue ;
-		}
+		handle_command(line, envp);//new
+		if (!line)
+			break ;
 	}
 	return (0);
 }
