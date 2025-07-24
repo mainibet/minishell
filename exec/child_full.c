54fe681f5b7e21541856ecd6ca972f3178f56e20
Alicia Betancourt Mainieri <albetanc@c4a13c6.42berlin.de>
2025-07-24 09:35:40 +0200

    working on pipe execution

---
+++ exec/child_full.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   child_full.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/06 12:07:44 by albetanc          #+#    #+#             */
+/*   Updated: 2025/07/08 16:42:49 by albetanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	child_process(t_node *node)
+{
+	t_fd_dup		dup;
+	t_cmd_data		*cmd;
+
+	cmd = &node->u_data.cmd;
+	if (setup_redir(cmd->fd_in, cmd->fd_out, &dup) != 0)
+		exit(1);
+	// if (&node->u_data.cmd == BUILTIN)//later this but if is not in pipe
+	// {
+	// 	execute_builtin(&node->u_data.cmd);//PENDING THOURGH IF COND WITH STRNCMP
+	// 	return (0);//check the return
+	// }
+	exec_external_cmd(node);//call this from child_process
+	perror (BOLD RED "Exec/Builtin failed" RESET);//this will happen if execution fails
+	cleanup_fd(node, node->type);
+	exit(EXIT_FAILURE);
+}
+
+/*
+*   Set-up the first cmd's execution in the pipeline
+*   1. It closes the unused read-end of the pipe
+*   2. Redirects the standard input from data->fd_in (the initial input file) if applies
+*   3. Directs its standard output to the write-end of the pipe (data->pipefd[1])
+*   4. Calls child_process to execute the command
+*/
+void	child_first(t_node *node, int prev_pipefd[2], t_initial_fd ini_fd)
+{
+	close_fd(data -> pipefd[0]);
+	fd = (t_initial_fd){data->fd_in, data->pipefd[1]};
+	child_process(node, type);
+}
+/*
+*   set-up middle cmds in the execution pipeline
+*/
+void    child_middle(t_node *node)
+{
+	int	*fd;//if so need to check that but later (other version)
+
+    fd = (t_initial_fd){data->pipefd[0], data->pipefd[1]};
+	child_process(node, type);
+}
+/*
+*   To setup the second (and final) cmd's execution in the pipeline.
+*   Closes the unused write-end of the pipe and the initial input file.
+*   Redirects standard input from the read-end of the pipe (data->pipefd[0])
+*   Directs its standard output to the newly opened output file.
+*   Calls child_process to execute 
+*/
+void	child_last(t_node *node)
+{
+	int				fd_out;
+
+	close(data -> pipefd[1]);
+	close(data -> fd_in);
+	fd_out = open(data -> argv[data -> argc - 1], O_WRONLY 
+			| O_CREAT | O_TRUNC, 0644);//this later only if there is a redirection in the args
+	if (fd_out == -1)
+	{
+		perror ("open output file");
+		exit (-1);
+	}
+	fd = data -> pipefd[0], fd_out;
+	child_process(node, type);
+}
---
+++ exec/parent_full.c
@@ -0,0 +1,144 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parent_full.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 16:15:44 by albetanc          #+#    #+#             */
+/*   Updated: 2025/07/08 18:18:47 by albetanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include "exec.h"//temporary for testing
+
+/*
+*   Parent waits for a specific child process to terminate, using waitpid()
+*   To clean up after a child and retrieve its exit status
+*/
+int	wait_one_child(pid_t pid, int *status)
+{
+	if (pid <= 0)//not wait if there is not valid pid
+		return (0);
+	if (waitpid(pid, status, 0) == -1)
+	{
+		perror (BOLD RED "Error waiting for child" RESET);
+		return (-1);
+	}
+	return (0);
+}
+
+int	wait_children(pid_t *pid, int nb_child, int *status)
+{
+	int	i;
+	int	res;
+
+	i = 0;
+	while (i < nb_child)
+	{
+		res = wait_one_child(pid[i], &status[i]);
+		if (res == -1)
+			return (-1);//check if smt else needed
+		i++;
+	}
+	return (0);
+}
+
+/*
+*   [ERROR HANDLING]
+*   Checks the result of a fork_handle
+*   If fork() failed, it might wait for a previously launched child 
+*   before returning an error status.
+*   To managing the parent's flow after a fork attempt.
+*/
+
+int	check_fork(int result, pid_t pid, int *status)//may be not needed with multiple child
+{
+	if (result != 0)
+	{
+		if (result == 1)
+			return (0);
+		else
+		{
+			if (pid != 0)
+				wait_child(pid, status);
+			return (result);
+		}
+	}
+	return (0);
+}
+
+/*
+*   Forks a new process
+*   1. Handles fork errors
+*   2. Executes child based on cmd position in a pipeline.
+*   3. Exit in case of non sucess in any function to end correctly child
+*/
+//i_cmd == 0 is first cmd
+//i_cmd == nb_cmd -1 last cmd
+//return pid to the parent
+//if pid fails not fd where inheritated, the parent handles them
+//pid > 0 we are in the parent process
+// pid == 0 we are in the child process
+// pid == -1 error creating child
+int	fork_handle(t_node *node, int i_cmd, int nb_cmd)
+{
+	pid_t pid;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror (BOLD RED "Fork failed" RESET);
+		cleanup_fd(node, node->type);//check if needed if smt came opened
+		return (-1);//check if smt should be free
+	}
+	if (pid == 0)
+	{
+		if (nb_cmd == 1)
+			child_process(node);//single cmd
+		else if (i_cmd == 0)
+			child_first(node);
+		else if (i_cmd == nb_cmd -1)
+			child_last(node);
+		else
+			child_middle(node);
+		exit(EXIT_FAILURE);
+	}
+	return (pid);
+}
+
+/*
+*   Parent
+*   Execute single external cmd
+*   1. find absolut path
+*   2. 
+*/
+void	execute_cmd(t_node *node)
+{
+	pid_t	pid;//use later w pipes
+    //char	*cmd_name; //when parsing args
+	int		child_status;
+	int		fork_status;
+
+	// if (&node->u_data.cmd == BUILTIN)//later this but if is not in pipe
+	// {
+	// 	execute_builtin(&node->u_data.cmd);//PENDING THOURGH IF COND WITH STRNCMP
+	// 	return ;//check the return
+	// }
+	// fork_status = fork_handle(pid, node->type, int i_cmd, int nb_cmd);//formultiple cmd?
+	fork_status = fork_handle(pid, node->type, 0, 1);//1 cmd v1
+	if (check_fork(fork_status, pid, &child_status))
+		return (fork_status);
+	cleanup_fd(node, node->type);//to close fd_in, pipefd[0] and pipefd[1]
+	if (wait_child_status(pid, &child_status) == -1)//this eventually should wait all children
+	{
+		perror (BOLD RED "Waitpid failed for child" RESET);
+		return (-1);
+	}
+	if (cleanup_cmd_node(node))//check if here or in the child
+		perror(MAGENTA "Failed to cleanup cmd node\n" RESET);
+	cleanup_fd(node, node->type);//check if here or in other place 
+    // exec_external_cmd(node);//call this from child_process
+}
+
