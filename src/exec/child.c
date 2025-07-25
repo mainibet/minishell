b61f9093fa03387254cb04b4970c10c03d6d780d
Alicia Betancourt Mainieri <albetanc@c4a13c4.42berlin.de>
2025-07-25 12:32:48 +0200

    Feat(child.c): fixed to begin with pipies

--- src/exec/child.c
+++ src/exec/child.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 12:07:44 by albetanc          #+#    #+#             */
-/*   Updated: 2025/07/23 14:23:45 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/07/25 10:59:55 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,19 @@
 */
 void	child_process(t_node *node)
 {
-	// t_fd_dup		dup;
+	t_fd_dup		dup;
 	t_cmd_data		*cmd;
 
 	cmd = &node->u_data.cmd;
-	// if (setup_redir(cmd->fd_in, cmd->fd_out, &dup) != 0)//only when actual redirectio is needed or in pipes
-	// 	exit(1);
+	if (setup_redir(cmd->fd_in, cmd->fd_out, &dup) != 0)//only when actual redirectio is needed or in pipes
+		exit(1);
+	// if (is_builtin(cmd->argv[0]))//might change if declared $ARG in cmd line
+	// {
+	// 	execute_builin(node);
+	// 	exit();//some exit status
+	// }
 	exec_external_cmd(node);
 	perror (BOLD RED "Exec/Builtin failed" RESET);
-	// cleanup_fd(node, node->type);// Only cleanup FDs opened by this child.
+	// cleanup_fd(node, node->type);// Only cleanup FDs opened by this child. CHECK IF W OTHERS
 	exit(EXIT_FAILURE);
 }
