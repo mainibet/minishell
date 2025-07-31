f8d429a295316e9408c343b41c774459dbb2a419
Alicia Betancourt Mainieri <albetanc@c1b4c3.42berlin.de>
2025-07-31 18:38:59 +0200

    feat(child.c): fixed to support pipies

--- src/exec/child.c
+++ src/exec/child.c
@@ -6,14 +6,30 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 12:07:44 by albetanc          #+#    #+#             */
-/*   Updated: 2025/07/25 10:59:55 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/07/31 18:32:11 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include "minishell.h"
 #include "../include/minishell.h"
 #include "../include/exec.h"
+#include "../include/builtin.h"
 
+//this is in child from pipes so no fork need
+int	execute_cimple_cmd(t_node *cmd_node)
+{
+	if (is_builtin(cmd_node->u_data.cmd.argv[0]))
+	{
+		my_echo(cmd_node);//in the meantime only with this
+		return (0);
+	}
+	else
+	{
+		exec_external_cmd(cmd_node);
+		exit (EXIT_FAILURE);
+	}
+	return (1);
+}
 /**
 *   @brief sets up I/O redi and executes cmd
 *
@@ -30,17 +46,22 @@ void	child_process(t_node *node)
 {
 	t_fd_dup		dup;
 	t_cmd_data		*cmd;
+	t_builtin_type	builtin_id;
+	int				status;
 
 	cmd = &node->u_data.cmd;
-	if (setup_redir(cmd->fd_in, cmd->fd_out, &dup) != 0)//only when actual redirectio is needed or in pipes
-		exit(1);
-	// if (is_builtin(cmd->argv[0]))//might change if declared $ARG in cmd line
-	// {
-	// 	execute_builin(node);
-	// 	exit();//some exit status
-	// }
-	exec_external_cmd(node);
-	perror (BOLD RED "Exec/Builtin failed" RESET);
-	// cleanup_fd(node, node->type);// Only cleanup FDs opened by this child. CHECK IF W OTHERS
-	exit(EXIT_FAILURE);
+	// if (setup_redir(cmd->fd_in, cmd->fd_out, &dup) != 0)//only when actual redirectio is needed or in pipes
+	// 	exit(1);
+	if (is_builtin(cmd->argv[0]))//might change if declared $ARG in cmd line
+	{
+		status = execute_builin(node);
+		exit(status);//some exit status
+	}
+	else
+	{
+		exec_external_cmd(node);//this was to execute only single external cmd
+		perror (BOLD RED "Exec/Builtin failed" RESET);
+		// cleanup_fd(node, node->type);// Only cleanup FDs opened by this child. CHECK IF W OTHERS
+		exit(EXIT_FAILURE);
+	}
 }
