b5ab56d59a2875d6a672e729e6b3739db2e2f0ae
Alicia Betancourt Mainieri <albetanc@c1b4c3.42berlin.de>
2025-08-01 09:41:05 +0200

    Feat: ft_strcmp new for builtins

---
+++ src/builtin/ft_strcmp.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 08:52:49 by albetanc          #+#    #+#             */
+/*   Updated: 2025/08/01 08:56:11 by albetanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int ft_strcmp(const char *s1, const char *s2)//include in libft
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char) s2[i]);
+}
