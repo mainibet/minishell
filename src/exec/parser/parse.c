a54ba78037746845ec0de8cad2bad0e0b27d0e4e
Alicia Betancourt Mainieri <albetanc@c1b4c3.42berlin.de>
2025-08-06 10:47:05 +0200

    Deleted testing functions for pipes execution

--- src/exec/parser/parse.c
+++ src/exec/parser/parse.c
@@ -173,63 +173,6 @@ t_node	*parse_command(t_token *token, char *path, char **argv, int *fd)
 	
 int	get_return_code(int wstatus);
 
-static t_token *find_lowest_precedence_operator(t_token *token)//ONLY TEST
-{
-    t_token *lowest_op;
-    t_token *current;
-    
-    lowest_op = NULL;
-    current = token;
-    
-    while (current)
-    {
-        // En este if, buscamos el valor de precedencia MÁS BAJO.
-        // Si el operador actual no es un operador (precedencia 0), lo ignoramos.
-        if (precedence(current) > 0 && (!lowest_op || precedence(current) < precedence(lowest_op)))
-        {
-            lowest_op = current;
-        }
-        current = current->next;
-    }
-    return (lowest_op);
-}
-
-t_node *parse(t_token *token_list)//SOLO TEST
-{
-    t_token *op_token;
-    t_token *current;
-    t_node  *left;
-    t_node  *right;
-
-    if (!token_list)
-        return (NULL);
-    
-    op_token = find_lowest_precedence_operator(token_list);
-
-    if (!op_token)
-        return (parse_command(token_list));
-
-    // Corrección aquí:
-    // Creamos la lista para la parte derecha, comenzando después del operador.
-    t_token *right_list = op_token->next;
-    
-    // Y ahora, para el lado izquierdo, recorremos la lista hasta el operador
-    // y rompemos el enlace, creando una lista de tokens para el lado izquierdo.
-    current = token_list;
-    while (current && current->next != op_token)
-        current = current->next;
-    
-    // Si encontramos el token antes de op_token, rompemos la lista.
-    if (current)
-        current->next = NULL;
-    
-    // Parseamos recursivamente el lado izquierdo y el derecho con las listas correctas.
-    left = parse(token_list);
-    right = parse(right_list);
-
-    // Unimos los resultados con el operador.
-    return (parse_operator(op_token, left, right));
-}
 /* execute the command or builtin specified by tokens
  * - the child (if any) should close each element of fd that is greater than 2
  * 		(not stdout, stdin, or stderr)
