07afcbf5da63c8c899d590e24c8729552018ab5c
Your Name <you@example.com>
2025-08-07 09:45:11 +0200

    commented update_proto and added origin of cproto image

--- util/update_proto.sh
+++ util/update_proto.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-[[ -z "$1" ]] && exit 1
-which cproto > /dev/null || alias cproto="docker run cproto -v $PWD:/cproto"
+# the header should have the line:
+# /* prototypes */
+# after the header protection, the #includes, the #defines, the structures, etc.
+# but before #endif
+
+[[ -z "$1" ]] && echo "usage: $0 <header>.h"
+which cproto > /dev/null || 
+	alias cproto="docker run jbarratt42/cproto \
+		-v $PWD:/cproto 
+		-v /usr/include:/usr/include"
 tmp=$(mktemp)
 sed '/\/\* prototypes \*\//q' $1 > $tmp
 cproto -F"int\tf(a, b)" *.c | \
