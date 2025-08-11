0685b7f29f84d20c66b90637b1014d58f8535daf
Alicia Betancourt Mainieri <albetanc@c1b4c3.42berlin.de>
2025-08-11 14:18:08 +0200

    Feat(exit.c): new builtin exit need a struct with program info with last status

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,25 +6,15 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 11:52:09 by albetanc          #+#    #+#             */
-/*   Updated: 2025/08/11 12:27:44 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/08/11 13:59:45 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "exec.h"//TMP FOR TESTING
 
-typedef struct s_minishell//new include in minishell.h
-{
-	//char *line;
-	//char	**envp_cpy;//uncomment when is in minishell.h
-	//char	**envp;//original not will be free
-	//t_node	*root;
-	//t_token	*token_list;
-	int		last_exit_status;
-}	t_program;
-
 //line included to centralized cleanup
-void	cleanup_program(t_program *program)//moved to cleanup.c
+void	cleanup_program(t_context *program)//moved to cleanup.c
 {//probably includes prompt when is dynamic
 	if (program->line)
 		free(program->line);
@@ -40,7 +30,7 @@ void	cleanup_program(t_program *program)//moved to cleanup.c
 *	@brief ends the progam and free resources needed: nodes, tokens, etc
 *	@returns void because ends the program
 */
-void	my_exit(t_program *program)
+void	my_exit(t_context *program)
 {
 	cleanup_program(program);
 	exit(program->last_exit_status);
