7fd1cf9d04a954fd393c9d4c79cd18a864072c0a
Alicia Betancourt Mainieri <albetanc@c1b4c3.42berlin.de>
2025-08-11 14:17:31 +0200

    Feat(env.c): recevied program with envp_cpy from a new structure created for exit builtin

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 09:55:37 by albetanc          #+#    #+#             */
-/*   Updated: 2025/08/11 09:59:57 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/08/11 13:59:45 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,21 @@
 #include "exec.h"//TMP FOR TESTING
 
 //return 0 for success
-int	my_env(char **envp)
+// int	my_env(char **envp)
+int	my_env(t_context *program, t_node *node)
 {
 	int	i;
 
-	if (!envp)
+	(void) node;//check
+	if (!program->envp_cpy)
 	{
 		fprintf(stderr, BLUE "env: no environment variables found\n");
 		return (1);
 	}
 	i = 0;
-	while (envp[i])
+	while (program->envp_cpy[i])
 	{
-		printf("%s\n", envp[i]);
+		printf("%s\n", program->envp_cpy[i]);
 		i++;
 	}
 	return (0);
