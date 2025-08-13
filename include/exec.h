2a9f8054d36a243118a2a7107d5433ac6b2632ab
Alicia Betancourt <rococo@Alicias-MacBook-Air.local>
2025-08-13 17:08:57 +0200

    Feat(exec.h): included cleanup_program prototype for my_exit flow

--- include/exec.h
+++ include/exec.h
@@ -63,6 +63,7 @@ int		wait_children(pid_t left_pid, pid_t right_pid, int *right_status);
 // int		cleanup_fd(t_node *node, t_node_type type);
 int		cleanup_fd(t_node *node, int node_type);
 int		cleanup_cmd_node(t_node *node);
+void	cleanup_program(t_program *program);
 
 /**
 Left-associative
