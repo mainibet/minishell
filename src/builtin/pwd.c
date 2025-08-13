c1b04b04bdc1b831b8c069066cba123838eaa583
Alicia Betancourt <rococo@Alicias-MacBook-Air.local>
2025-08-13 18:07:53 +0200

    Feat(pwd.c): included same parameter format as others builtins

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -13,11 +13,33 @@
 #include "minishell.h"
 #include "exec.h"//TMP FOR TESTING
 
+static char	*find_env_value(char **envp, const char *key)
+{
+	int	i;
+	int	len;
+
+	if (!envp || !key)
+		return (NULL);
+	len = ft_strlen(key);
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], key, len) == 0 && envp[i][len] == '=')
+			return (&envp[i][len + 1]);
+		i++;
+	}
+	return (NULL);
+}
+
 //cwd: current working directory
 //Original pwd: Print the full 
 //filename of the current working directory.
-int	my_pwd(char *cwd_path)
+int	my_pwd(t_program *program, t_node *node)
 {
+	char	*cwd_path;
+
+	(void) node;
+	cwd_path = find_env_value(program->envp_cpy, "PWD");
 	if (cwd_path)
 	{
 		printf("%s\n", cwd_path);
