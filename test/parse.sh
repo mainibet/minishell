7583c81333b3021697246e940deaeb8ea5b59509
Alicia Betancourt <141577801+mainibet@users.noreply.github.com>
2025-08-13 13:11:27 +0000

    Clean up files not needed

--- test/parse.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cc parse.c -o parse && ./parse "c0 | c1 ; c2 | c3 | c4 ; c5 | c6 | c7"
--- test/pipeline.c
+++
@@ -1,9 +0,0 @@
-#include "../src/pipeline.c"
-
-int	main(void)
-{
-	char *paths[] = {"/usr/bin/cat", "/usr/bin/cat", "./hello.sh", NULL};
-	pipeline(paths, NULL);
-	return 0;
-}
-
