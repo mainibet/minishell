e4c29285458b410aa414b67779adb8d01aeea520
Alicia Betancourt <rococo@Alicias-MacBook-Air.local>
2025-08-14 16:15:24 +0200

    Style(pipe.c): deleted comments

--- src/exec/pipe.c
+++ src/exec/pipe.c
@@ -11,67 +11,9 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include "exec.h"//temporary for testing
+#include "exec.h"
 
-/*
-	Used in pipes to count pipe nodes
-	1. loop in the nodes until match the type node chosen
-	2. count the nodes of that type
-	Return the count
-	Note: the fisrt if condition is the base case of the recursion
-*/
-// int count_node(t_node *node, t_nodetype target_type)//NEW
-// {
-// 	int	count;
-
-// 	if (node == NULL) 
-// 		return (0);
-// 	count = 0;
-// 	if (node->type == target_type)
-// 		count = 1;
-// 	count += count_node(node->u_data.op.left, target_type);
-// 	count += count_node(node->u_data.op.right, target_type);
-// 	return (count);
-// }
-
-/*
-	Setup a pipe between commands.
-	Returns 0 on success, -1 on failure.
-	Closes the input file descriptor if pipe creation fails.
-*/
-// int	setup_pipe(int pipefd[2], int fd_in)//check if static or not
-// {
-// 	if (pipe(pipefd) == -1)
-// 	{
-// 		perror(BOLD RED "Pipe failed\n" RESET);
-// 		close_fd(fd_in);
-// 		return (-1);
-// 	}
-// 	return (0);
-// }
-
-/*
-*   Orchestrates the overall execution flow in the parent process for the current two-command pipex setup
-*   1. Get how many cmd are to create same nb of child processes
-*/
-//check how many childs are: how many pipe nodes, so it will create 2 child pero pipe.
-//n cmds need n-1 pipes
-//create fork per each child
-// int	parent(struct s_pipe_data *data)
-// is_pipeline() helper? to mark the cmd?
-
-	// pid_t	pid;
-	// int		child_status;
-	// int		fork_status;
-
-	// fork_status = fork_handle(&pid, node, 0, 1);
-	// if (check_fork(fork_status, pid, &child_status))
-	// 	return (fork_status);
-	// cleanup_fd(node, node->type);//include condition only for fd bigger than 2
-	// if (wait_one_child(pid, &child_status) == -1)
-	// return (-1);
-
-	int	wait_children(pid_t left_pid, pid_t right_pid, int *right_status)//NEW
+int	wait_children(pid_t left_pid, pid_t right_pid, int *right_status)
 {
 	int	left_status;
 
@@ -131,14 +73,9 @@ pid_t	execute_right(t_program *program, t_node *right_node, int pipefd[2])
 	}
 	return (pid);
 }
-/*
-* Creates pipe (setup_pipe)
-* Do forks (check_fork)
-* Executes each child in a fork
-*/
 
 //this consider left and right child
-int	execute_pipeline(t_program *program, t_node *node)
+int	execute_pipeline(t_program *program, t_node *node)//make it shorter
 {
 	pid_t	left_pid;
 	pid_t	right_pid;
