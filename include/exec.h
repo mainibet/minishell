6f31e16a7c3b0ada65955ed4234bcd13d5923ec9
Alicia Betancourt <rococo@Alicias-MacBook-Air.local>
2025-08-14 16:16:52 +0200

    Style(exec.h): deleted comments not needed

--- include/exec.h
+++ include/exec.h
@@ -42,12 +42,12 @@ char	*find_path(char *argv);
 // --- EXECUTION --- //
 //Parent cmd node
 // int		wait_one_child(pid_t pid, int *status);//included in exec_cmd_nopipe
-int		fork_handle(pid_t *pid, t_node *node, int i_cmd, int nb_cmd);
+int		fork_handle(pid_t *token_headid, t_node *node, int i_cmd, int nb_cmd);
 int		setup_redir(int fd_in, int fd_out, t_fd_dup *dup);
 int		execution(t_program *program, t_node *node, bool is_pipe_child);
 int		handle_cmd_exec(t_program *program, t_node *node, bool is_pipe_child);
 // int		execution(t_node *cmd);
-int		exec_cmd_nopipe(t_program *promgra, t_node *node);
+int		exec_cmd_nopipe(t_program *token_headromgra, t_node *node);
 // int		execute_simple_cmd(t_node *cmd_node);//when is in pipe to avoid double fork
 void	exec_cmd_inpipe(t_node *node);
 
@@ -57,7 +57,7 @@ void child_process(t_program *program, t_node *node);
 // void	exec_external_cmd(t_node *node);
 
 //pipelines
-int		execute_pipeline(t_node *node);
+int	execute_pipeline(t_program *program, t_node *node);
 int		wait_children(pid_t left_pid, pid_t right_pid, int *right_status);
 
 /**
