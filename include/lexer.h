519da2e093e324276b44de60376de0b485a204a9
Alicia Betancourt Mainieri <albetanc@c1b4c3.42berlin.de>
2025-08-23 11:39:21 +0200

    Style(lexer.h): making norm happy with tab

--- include/lexer.h
+++ include/lexer.h
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 08:36:45 by albetanc          #+#    #+#             */
-/*   Updated: 2025/07/23 12:00:07 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/08/23 11:38:58 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@
 // -----------------------------------------//
 
 t_token			*lex(char *s, char delim);
-enum e_toktype token_type(char *s);
-char	*consume_whitespace(char *head_token);
-int is_operator_char(char c);
-t_token *lex_quoted(char *s, char quote);
-t_token *lex_unquoted(char *s);
+enum e_toktype	token_type(char *s);
+char			*consume_whitespace(char *head_token);
+int				is_operator_char(char c);
+t_token			*lex_quoted(char *s, char quote);
+t_token			*lex_unquoted(char *s);
 t_token			*extract_token(char *s, size_t size);
 
 #endif
