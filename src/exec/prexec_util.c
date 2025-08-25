8596a5f412c6fc478d8ff8b78265049ad30b0a9a
mainibet <mainibet@gmail.com>
2025-08-25 10:56:36 +0200

    Fix(prexec_util.c): deleted call to process_redir in apply_redir to not open files in preexecution

--- src/exec/prexec_util.c
+++ src/exec/prexec_util.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 10:27:01 by albetanc          #+#    #+#             */
-/*   Updated: 2025/08/23 11:14:59 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/08/25 10:55:24 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -131,12 +131,5 @@ void	apply_redir(t_program *program, t_node *node)
 			exit(EXIT_FAILURE);
 		}
 		redir = redir->next;
-	}
-	if (process_redir(&node->u_data.cmd) != 0)
-	{
-		fprintf(stderr, BOLD RED "Redir error in preexec '%s'\n" RESET,
-			node->u_data.cmd.argv[0]);
-		cleanup_program(program);
-		exit(EXIT_FAILURE);
 	}
 }
