3090db19c200d1595a698682f61990b69a77c483
mainibet <mainibet@gmail.com>
2025-08-25 13:30:55 +0200

    Feat(pipe.c): updated pipefd[2] in each cmd struct

--- src/exec/pipe.c
+++ src/exec/pipe.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 12:59:38 by albetanc          #+#    #+#             */
-/*   Updated: 2025/08/23 12:01:14 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/08/25 13:30:05 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 
 //uses pipdefd[1] to write
-pid_t	execute_left(t_program *program, t_node *left_node, int pipefd[2])
+pid_t	execute_left(t_program *program, t_node *left_node)
 {
 	pid_t	pid;
 
@@ -26,20 +26,20 @@ pid_t	execute_left(t_program *program, t_node *left_node, int pipefd[2])
 	}
 	else if (pid == 0)
 	{
-		if (dup2(pipefd[1], STDOUT_FILENO) == -1)
+		if (dup2(left_node->u_data.cmd.pipefd[1], STDOUT_FILENO) == -1)
 		{
 			perror("Error: dup2 failed for left cmd");
 			exit(1);
 		}
-		close_fd(&pipefd[0]);
-		close_fd(&pipefd[1]);
+		close_fd(&left_node->u_data.cmd.pipefd[0]);
+		close_fd(&left_node->u_data.cmd.pipefd[1]);
 		execution(program, left_node, true);
 		exit(EXIT_FAILURE);
 	}
 	return (pid);
 }
 
-pid_t	execute_right(t_program *program, t_node *right_node, int pipefd[2])
+pid_t	execute_right(t_program *program, t_node *right_node)
 {
 	pid_t	pid;
 
@@ -51,13 +51,13 @@ pid_t	execute_right(t_program *program, t_node *right_node, int pipefd[2])
 	}
 	else if (pid == 0)
 	{
-		if (dup2(pipefd[0], STDIN_FILENO) == -1)
+		if (dup2(right_node->u_data.cmd.pipefd[0], STDIN_FILENO) == -1)
 		{
 			perror("Error: dup2 failed for right cmd");
 			exit(1);
 		}
-		close_fd(&pipefd[0]);
-		close_fd(&pipefd[1]);
+		close_fd(&right_node->u_data.cmd.pipefd[0]);
+		close_fd(&right_node->u_data.cmd.pipefd[1]);
 		execution(program, right_node, true);
 		exit(EXIT_FAILURE);
 	}
@@ -86,14 +86,16 @@ int	execute_pipeline(t_program *program, t_node *node)
 		perror("Error: Pipe failed");
 		return (1);
 	}
-	pids[0] = execute_left(program, node->u_data.op.left, pipefd);
+	node->u_data.op.left->u_data.cmd.pipefd[1] = pipefd[1];
+	node->u_data.op.right->u_data.cmd.pipefd[0] = pipefd[0];
+	pids[0] = execute_left(program, node->u_data.op.left);
 	if (pids[0] == -1)
 	{
 		close_all_pipefd(&pipefd[0], &pipefd[1]);
 		return (1);
 	}
 	close_fd(&pipefd[1]);
-	pids[1] = execute_right(program, node->u_data.op.right, pipefd);
+	pids[1] = execute_right(program, node->u_data.op.right);
 	if (pids[1] == -1)
 	{
 		close_fd(&pipefd[0]);
