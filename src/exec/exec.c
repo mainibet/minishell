60a30f2888e482dae0e94897a047c3eca5e4de7b
mainibet <mainibet@gmail.com>
2025-08-26 15:39:27 +0200

    Removed old parts commneted

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 16:32:13 by albetanc          #+#    #+#             */
-/*   Updated: 2025/08/26 14:55:35 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/08/26 15:38:53 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,20 +76,16 @@ int	is_operator_str(const char *str)
 // connect pipes if there is no redir
 void	set_final_fds(t_cmd_data *cmd)
 {
-	// if (cmd->fd_in == STDIN_FILENO && cmd->pipefd[0] > 2)
 	if (cmd->fd_in == STDIN_FILENO && cmd->pipefd[0] >= 0)
 		cmd->fd_in = cmd->pipefd[0];
-	// if (cmd->fd_out == STDOUT_FILENO && cmd->pipefd[1] > 2)
 	if (cmd->fd_out == STDOUT_FILENO && cmd->pipefd[1] >= 0)
 		cmd->fd_out = cmd->pipefd[1];
-	// if (cmd->fd_in != STDIN_FILENO)
 	if (cmd->fd_in != STDIN_FILENO && cmd->fd_in >= 0)
 	{
 		redir_in(cmd->fd_in);
 		close_fd(&cmd->fd_in);
 	}
 	if (cmd->fd_out != STDOUT_FILENO && cmd->fd_out >= 0)
-	// if (cmd->fd_out != STDOUT_FILENO)
 	{
 		redir_out(cmd->fd_out);
 		close_fd(&cmd->fd_out);
