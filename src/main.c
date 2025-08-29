2bdae5dd718025658d5b4b99108009b6c28ba9f8
mainibet <mainibet@gmail.com>
2025-08-29 13:06:21 +0200

    Doc(main.c colors.h): clear comments

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 08:42:03 by albetanc          #+#    #+#             */
-/*   Updated: 2025/08/29 11:02:27 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/08/29 13:00:25 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ t_node	*token_parser_input(char *line, t_token **token_out, t_program *program)
 		*token_out = NULL;
 		return (NULL);
 	}
-	expand(token_list, program->envp_cpy, program->last_exit_status);   // Expansion Function <<<<<<-----------
+	expand(token_list, program->envp_cpy, program->last_exit_status);
 	parser_tokens = token_list;
 	root = parse(parser_tokens);
 	if (!root)
@@ -55,15 +55,13 @@ static void	process_cmdline(t_program *program, char *line)
 	root = token_parser_input(line, &program->token_list, program);
 	program->root = root;
 	free(line);
-	if (!program->root)//if parsing failed
+	if (!program->root)
 	{
 		fprintf(stderr, RED "Parsing failed\n" RESET);
 		return ;
 	}
 	DEBUG_PRINT(stderr, BOLD MAGENTA "AST built. stating pre-execution \n" RESET);//TEST
-	// print_ast(root, 0); // might be useful for debugging
-	DEBUG_PRINT_AST(root);
-	// fprintf(stderr, BOLD MAGENTA "AST built. stating pre-execution \n" RESET);//TEST
+	DEBUG_PRINT_AST(root);//test
 	pre_execution(program, root);
 	program->last_exit_status = execution(program, root, false);
 	free_ast_tokens(program);
@@ -72,7 +70,7 @@ static void	process_cmdline(t_program *program, char *line)
 int	main(int argc, char **argv, char **envp)
 {
 	char		*prompt;
-	t_program	program;//new for builtin exit
+	t_program	program;
 
 	(void) argc;//check if needed
 	(void) argv;//check if needed
