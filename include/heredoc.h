5b21945b7b3c18eb8f6b776c8c8b42470d5c66a2
mainibet <mainibet@gmail.com>
2025-09-07 17:31:16 +0200

    Style(heredoc. eredoc.h): refactored heredoc in smaller functions

--- include/heredoc.h
+++ include/heredoc.h
@@ -1,11 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/07 16:37:46 by albetanc          #+#    #+#             */
+/*   Updated: 2025/09/07 17:28:02 by albetanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef HEREDOC_H
-#define HEREDOC_H
+# define HEREDOC_H
 
 # include "minishell.h"
-int is_quoted(const char *s);
-char *strip_outer_quotes(const char *s);
-// int	heredoc_prepare(t_redir *redir, char **envp_cpy, int last_exit);
-int	heredoc_prepare(t_redir *redir, t_program *program);
+
+typedef struct s_heredoc
+{
+	t_redir		*redir;
+	t_program	*program;
+	char		*buf;
+	int			pipefd[2];
+	int			current_line;
+	char		*delimiter;
+}	t_heredoc;
+
+int		heredoc_prepare(t_redir *redir, t_program *program);
 void	heredoc_normalize_delimiter(t_redir *redir);
+int		is_quoted(const char *s);
+char	*strip_outer_quotes(const char *s);
 
 #endif
--- src/expansion/heredoc.c
+++ src/expansion/heredoc.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/07 17:29:36 by albetanc          #+#    #+#             */
+/*   Updated: 2025/09/07 17:29:37 by albetanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "minishell.h"
 
@@ -54,184 +65,48 @@ void	heredoc_normalize_delimiter(t_redir *redir)
 	}
 }
 
-// /* prepare heredoc: create a pipe, read user input until delimiter */
-// // int heredoc_prepare(t_redir *redir, char **envp, int last_exit)
-// int	heredoc_prepare(t_redir *redir, t_program *program)
-// {
-// 	int		pipefd[2];
-// 	char	*line;
-// 	pid_t	pid;
-// 	int		status;
-// 	int		current_line;
-// 	char	*buf; //new
-
-// 	buf = NULL;
-// 	current_line = 1;
-// 	if (!redir || pipe(pipefd) == -1)
-// 	{
-// 		perror("heredoc: pipe failed");
-// 		return (1);
-// 	}
-// 	// Fork a child process for the heredoc
-// 	pid = fork();
-// 	if (pid < 0)
-// 	{
-// 		perror("heredoc: fork failed");
-// 		close_fd(&pipefd[0]);
-// 		close_fd(&pipefd[1]);
-// 		return (1);
-// 	}
-// 	if (pid == 0)
-// 	{
-// 	// Child process
-// 		close_fd(&pipefd[0]); // Close read end in child
-// 		set_signal_heredoc(); // Set signal handlers for heredoc
-// 		g_signal_value = 0; // Reset signal value
-// 		heredoc_normalize_delimiter(redir);//new position
-// 		while (1) // Read lines until delimiter or signal
-// 		{
-// 			line = readline("> ");
-// 			if (!line)//needed EOF detected
-// 			{
-// 				if (g_signal_value == SIGINT)//needed
-// 				{
-// 					// free (line);
-// 					// free (buf);
-// 					close_fd(&pipefd[1]);
-// 					exit(130);//ctr+c
-// 				}
-// 				if (buf) // si ya escribimos algo en buf, mandarlo al pipe
-// 				 {
-// 					char *to_write = buf;
-// 					if (redir->hd_expand) // expandir si corresponde
-// 					{
-// 						to_write = expand_token_text(buf, program->envp_cpy, program->last_exit_status);
-// 						// free(buf);
-// 						free (line);
-// 					}
-// 					write(pipefd[1], to_write, ft_strlen(to_write));
-// 					// free(to_write);
-// 					if (redir->hd_expand)
-// 						free(to_write);
-// 					else
-// 						free(buf);
-// 					line = NULL;//new
-// 					buf = NULL;
-// 				}
-// 				fprintf(stderr, "warning: here-document at line %d delimited by end-of-file (wanted `%s')\n",
-// 						current_line,
-// 						redir->target); //warning in ctr+D
-// 				// free(buf);not! makes double free
-// 				break;
-// 			}
-// 			if (is_delim_line(line, redir->target))	// Check for delimiter
-// 			{
-// 				free(line);
-// 				break ;
-// 			}
-// 			//append line
-// 			char *tmp = buf;//concatenate buf
-// 			buf = buf ? ft_strjoin(buf, line) : ft_strdup(line);//check handle malloc and free
-// 			free(tmp);
-// 			// free(to_write);
-// 			//add new line
-// 			tmp = buf;
-// 			buf = ft_strjoin(buf, "\n");
-// 			free (tmp);
-// 			free (line);
-// 			current_line++;//new for warning message
-// 		}
-// 		// Process the line and expand if needed
-// 		if (redir->hd_expand && buf)
-// 		{
-// 			char *expanded = expand_token_text(buf, program->envp_cpy, program->last_exit_status);
-// 			free(buf);
-// 			buf = expanded;
-// 		}		// Write in pipe, clean up and exit
-// 		if (buf)
-// 		{
-// 			write(pipefd[1], buf, ft_strlen(buf));
-// 			free(buf);
-// 		}
-// 		close_fd(&pipefd[1]);
-// 		exit(0);
-// 	}
-// 	// Parent process
-// 	close_fd(&pipefd[1]);  // Close write end in parent
-// 	redir->fd = pipefd[0];  // Store read end for command
-// 	set_signal_prompt(1);//new
-// 	waitpid(pid, &status, 0);// Wait for child to finish
-// 	set_signal_prompt(0);//new
-// 	// If child was interrupted by Ctrl+C (status 130)
-// 	if ((WIFEXITED(status) && WEXITSTATUS(status) == 130)
-// 		|| (WIFSIGNALED(status) && WTERMSIG(status) == SIGINT))
-// 	{
-// 		g_signal_value = SIGINT;
-// 		close_fd(&pipefd[0]);
-// 		if (program->line)//new
-// 		{
-// 			free(program->line);//new
-// 			program->line = NULL;//new
-// 		}
-// 		rl_replace_line("", 0); // borra la línea actual en readline
-// 		rl_on_new_line(); // mueve readline a una nueva línea
-// 		rl_redisplay(); 
-// 		return (1);
-// 	}
-// 	// Handle other errors
-// 	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-// 	{
-// 		close_fd(&pipefd[0]);
-// 		return (1);
-// 	}
-// 	// Success
-// 	tcsetattr(STDIN_FILENO, TCSANOW, &program->orig_termios);//new -restore terminal signals ONLY IN PARENT
-// 	set_signal_prompt(0);
-// 	return (0);
-// }
-
-void	write_tmp_heredoc(t_redir *redir, t_program *program,
-	char **buf, char **line, int *pipefd)//fix param #
+// void	write_tmp_heredoc(t_redir *redir, t_program *program,
+// 	char **buf, char **line, int *pipefd)//fix param #
+void	write_tmp_heredoc(t_heredoc *hd, char **line)
 {
 	char	*to_write;
 
-	to_write = *buf;
-	if (redir->hd_expand)
+	to_write = hd->buf;
+	if (hd->redir->hd_expand)
 	{
-		to_write = expand_token_text(*buf, program->envp_cpy,
-				program->last_exit_status);
+		to_write = expand_token_text(hd->buf, hd->program->envp_cpy,
+				hd->program->last_exit_status);
 		if (line)
 		{
 			free (*line);
 			*line = NULL;
 		}
 	}
-	write(*pipefd, to_write, ft_strlen(to_write));
-	if (redir->hd_expand)
+	write(hd->pipefd[1], to_write, ft_strlen(to_write));
+	if (hd->redir->hd_expand)
 		free(to_write);
 	else
-		free(*buf);
+		free(hd->buf);
 	*line = NULL;
-	*buf = NULL;
+	hd->buf = NULL;
 }
 
 
 //concatenate buf in tmp later maybe in fd
-void	process_line_heredoc(t_redir *redir, t_program *program,
-		char **buf, char *line)
+void	process_line_heredoc(t_heredoc *hd, char *line)
 {
 	char	*tmp;
 
-	tmp = *buf;
-	if (*buf)
+	tmp = hd->buf;
+	if (hd->buf)
 	{
-		tmp = *buf;
-		*buf = ft_strjoin(*buf, line);
+		tmp = hd->buf;
+		hd->buf = ft_strjoin(hd->buf, line);
 	}
 	else
-		*buf = ft_strdup(line);
-	tmp = *buf;
-	*buf = ft_strjoin(*buf, "\n");
+		hd->buf = ft_strdup(line);
+	tmp = hd->buf;
+	hd->buf = ft_strjoin(hd->buf, "\n");
 	free (tmp);
 	free (line);
 }
@@ -247,18 +122,26 @@ void	expand_heredoc(t_program *program, char **buf)
 	*buf = expanded;
 }
 
-void	exit_ctrlc_heredoc(int *pipefd, char **line, char **buf)
+// void	exit_ctrlc_heredoc(int *pipefd, char **line, char **buf)
+void	exit_ctrlc_heredoc(t_heredoc *hd, char **line)
 {
 	if (line)
 		free (line);
-	if (buf && *buf)
-		free (*buf);
-	close_fd(&pipefd[1]);
+	if (hd->buf && hd->buf)
+		free (hd->buf);
+	close_fd(&hd->pipefd[1]);
 	exit(130);
 }
 
-int	read_heredoc(t_program *program, t_redir *redir,
-		char **buf, int *pipefd, int current_line)//fix # param
+char	*set_warn_ctrl_d(void)
+{
+	return ("warning: here-document at line %d "
+		"delimited by end-of-file (wanted `%s')\n");
+}
+
+// int	read_heredoc(t_program *program, t_redir *redir,
+// 		char **buf, int *pipefd, int current_line)//fix # param
+int	read_heredoc(t_heredoc *hd)
 {
 	char	*line;
 
@@ -268,25 +151,37 @@ int	read_heredoc(t_program *program, t_redir *redir,
 		if (!line)
 		{
 			if (g_signal_value == SIGINT)
-				exit_ctrlc_heredoc(pipefd, &line, &buf);
-			if (buf && *buf)
-				write_tmp_heredoc(redir, program, buf, &line, &pipefd[1]);
-			fprintf(stderr,
-				"warning: here-document at line %d "
-				"delimited by end-of-file (wanted `%s')\n",
-				current_line, redir->target);
+				exit_ctrlc_heredoc(hd, &line);
+			if (hd->buf)
+				write_tmp_heredoc(hd, &line);
+			fprintf(stderr, set_warn_ctrl_d(), hd->current_line, hd->delimiter);
+			// fprintf(stderr,
+			// 	"warning: here-document at line %d "
+			// 	"delimited by end-of-file (wanted `%s')\n",
+				// hd->current_line, hd->redir->target);
 			break ;
 		}
-		if (is_delim_line(line, redir->target))
+		if (is_delim_line(line, hd->redir->target))
 		{
 			free(line);
 			break ;
 		}
-		process_line_heredoc(redir, program, buf, line);
-
-		current_line++;
+		process_line_heredoc(hd, line);
+		hd->current_line++;
 	}
-	return (current_line);
+	return (hd->current_line);
+}
+
+void	init_hd(t_heredoc *hd, t_redir *redir,
+	t_program *program, int pipefd[2])
+{
+	hd->redir = redir;
+	hd->program = program;
+	hd->buf = NULL;
+	hd->pipefd[0] = pipefd[0];
+	hd->pipefd[1] = pipefd[1];
+	hd->current_line = 1;
+	hd->delimiter = redir->target;
 }
 
 // Close read end in child
@@ -298,23 +193,20 @@ int	read_heredoc(t_program *program, t_redir *redir,
 //process line heredoc
 void	child_heredoc(t_redir *redir, t_program *program, int pipefd[2])
 {
-	char	*buf;
-	int		current_line;
+	t_heredoc	hd;
 
-	buf = NULL;
-	current_line = 1;
-	close_fd(&pipefd[0]);
+	init_hd(&hd, redir, program, pipefd);
+	close_fd(&hd.pipefd[0]);
 	set_signal_heredoc();
 	g_signal_value = 0;
 	heredoc_normalize_delimiter(redir);
-	current_line = read_heredoc(program, redir, &buf,
-			pipefd, current_line);
-	if (redir->hd_expand && buf)
-		expand_heredoc(program, &buf);
-	if (buf)
+	hd.current_line = read_heredoc(&hd);
+	if (redir->hd_expand && hd.buf)
+		expand_heredoc(program, &hd.buf);
+	if (hd.buf)
 	{
-		write(pipefd[1], buf, ft_strlen(buf));
-		free(buf);
+		write(pipefd[1], hd.buf, ft_strlen(hd.buf));
+		free(hd.buf);
 	}
 	close_fd(&pipefd[1]);
 	exit(0);
