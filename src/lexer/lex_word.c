3a129334210ddc31e944768728586d4d9fbb17c7
mainibet <mainibet@gmail.com>
2025-09-09 16:01:57 +0200

    Style(ex_word.c): norm happy with all lexer/

--- src/lexer/lex_word.c
+++ src/lexer/lex_word.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 07:28:43 by albetanc          #+#    #+#             */
-/*   Updated: 2025/09/09 08:20:28 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/09/09 15:59:28 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,54 +18,85 @@ int	ft_isspace(char c)
 		|| c == '\v' || c == '\f' || c == '\r');
 }
 
-t_token	*lex_unquoted(char *s)
+// Advance to the end of an unquoted token
+// handle escape
+static char	*scan_unquoted_end(char *end)
 {
-	char	*start = s;
-	char	*end = s;
-	char	*rest;
-	t_token	*token;
-	t_token	*next;
-
-	while (*end && !ft_isspace(*end) && !is_operator_char(*end) && *end != '\'' && *end != '"')
+	while (*end && !ft_isspace(*end) && !is_operator_char(*end)
+		&& *end != '\'' && *end != '"')
 	{
-		if (*end == '\\' && *(end + 1)) // handle escape
-		end += 2;
+		if (*end == '\\' && *(end + 1))
+			end += 2;
 		else
 			end++;
 	}
+	return (end);
+}
+
+// Check for malformed redirection within the token
+static int	check_redir_syntax(t_token	*token)
+{
+	size_t	i;
+
+	i = 0;
+	while (token->txt[i])
+	{
+		if ((token->txt[i] == '>' || token->txt[i] == '<')
+			&& i > 0 && token->txt[i - 1] != ' ') 
+		{
+			fprintf(stderr, BOLD RED
+				"Syntax error: Invalid redirection syntax in '%s'\n"
+				RESET, token->txt);
+			free_token(token);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+// Process adjacent token without space
+static t_token	*handle_adj_token(t_token *token, char *rest)
+{
+	t_token	*next;
+
+	if (*rest == '\'' || *rest == '"')
+		next = lex_quoted(rest + 1, *rest);
+	else
+		next = lex_unquoted(rest);
+	if (!next)
+	{
+		free_token(token);
+		return (NULL);
+	}
+	return (join_tokens(token, next));
+}
+
+// Check for redirection characters within the word
+// If the word contains redirection chars, check if they're at positions where
+// they'd form valid redirection syntax (beginning of word or after space)
+// Check for adjacent token without whitespace
+t_token	*lex_unquoted(char *s)
+{
+	char	*start;
+	char	*end;
+	char	*rest;
+	t_token	*token;
+
+	start = s;
+	end = scan_unquoted_end(s);
 	token = extract_token(start, end - start);
 	if (!token)
 		return (NULL);
 	token->type = WORD;
-	// Check for redirection characters within the word
-	if (strchr(token->txt, '>') != NULL || strchr(token->txt, '<') != NULL) {
-		// If the word contains redirection chars, check if they're at positions where
-		// they'd form valid redirection syntax (beginning of word or after space)
-		for (size_t i = 0; token->txt[i]; i++)
-		{
-		if ((token->txt[i] == '>' || token->txt[i] == '<') && i > 0 && token->txt[i-1] != ' ') 
-			{
-				fprintf(stderr, BOLD RED "Syntax error: Invalid redirection syntax in '%s'\n" RESET, token->txt);
-				free_token(token);
-				return (NULL);
-			}
-		}
+	if (strchr(token->txt, '>') != NULL || strchr(token->txt, '<') != NULL)
+	{
+		if (!check_redir_syntax(token))
+			return (NULL);
 	}
 	rest = end;
-	// Check for adjacent token without whitespace
 	if (*rest && !ft_isspace(*rest) && !is_operator_char(*rest))
-	{
-		if (*rest == '\'' || *rest == '"')
-			next = lex_quoted(rest + 1, *rest);
-		else
-			next = lex_unquoted(rest);
-		if (!next)
-		{
-			free_token(token);
-			return (NULL);
-		}
-		token = join_tokens(token, next);
-	}
+		return (handle_adj_token(token, rest));
 	else
 		token->next = lex(consume_whitespace(rest), ' ');
 	return (token);
