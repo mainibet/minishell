5b672c8671ffb8d50bd2319b4fb15d89b59dfe34
mainibet <mainibet@gmail.com>
2025-09-10 08:45:44 +0200

    Style(cleanup.c) Cleanyo splitted in files

---
+++ src/utils/cleanup_node.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleanup_node.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/10 08:24:38 by albetanc          #+#    #+#             */
+/*   Updated: 2025/09/10 08:35:20 by albetanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	free_redirs(t_redir *redir)
+{
+	t_redir	*tmp;
+
+	while (redir)
+	{
+		tmp = redir->next;
+		free(redir->target);
+		free(redir);
+		redir = tmp;
+	}
+}
+
+void	free_cmd_arg(t_node *node)
+{
+	int	i;
+
+	i = 0;
+	while (node->u_data.cmd.argv[i])
+	{
+		free(node->u_data.cmd.argv[i]);
+		i++;
+	}
+	free(node->u_data.cmd.argv);
+	node->u_data.cmd.argv = NULL;
+}
+
+void	free_node(t_node *node)
+{
+	if (!node)
+		return ;
+	if (node->type == OPERATOR)
+	{
+		free_node(node->u_data.op.left);
+		free_node(node->u_data.op.right);
+	}
+	else if (node->type == COMMAND)
+	{
+		cleanup_cmd_node(node);
+		if (node->u_data.cmd.tokens)
+		{
+			free_token(node->u_data.cmd.tokens);
+			node->u_data.cmd.tokens = NULL;
+		}
+		if (node->u_data.cmd.argv)
+			free_cmd_arg(node);
+		if (node->u_data.cmd.redir)
+		{
+			free_redirs(node->u_data.cmd.redir);
+			node->u_data.cmd.redir = NULL;
+		}
+	}
+	free (node);
+}
