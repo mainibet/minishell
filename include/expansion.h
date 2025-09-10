38c9c2667090de160f7a331d3a78e5918bd4d24f
Tanmay Pandya <tanmaypandya159@gmail.com>
2025-09-10 14:09:36 +0200

    Norminetted whole src

--- include/expansion.h
+++ include/expansion.h
@@ -2,6 +2,10 @@
 # define EXPANSION_H
 # include "minishell.h"
 
+char	*get_env_value(const char *var, char **envp_cpy);
+char	*status_exp(char c, int last_exit);
+char	*expand_token_helper_01(char **pos, char **envp_cpy);
+char	*expand_token_helper(char *result, char *pos);
 void	expand(t_token *token, char **envp_cpy, int last_exit);
 char	*expand_token_text(char *txt, char **envp_cpy, int last_exit);
 
