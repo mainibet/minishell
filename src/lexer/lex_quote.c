97cf18e53e57f815c13a653e32be59d54c790980
mainibet <mainibet@gmail.com>
2025-09-12 16:35:47 +0200

    Changed error message function

--- src/lexer/lex_quote.c
+++ src/lexer/lex_quote.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 07:28:27 by albetanc          #+#    #+#             */
-/*   Updated: 2025/09/12 16:32:26 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/09/12 16:35:24 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,7 +90,7 @@ t_token	*lex_quoted(char *s, char quote)
 	end = find_quote_end(s, quote);
 	if (!*end)
 	{
-		ft_print_syntax_error("unclosed quote");
+		ft_print_error("Syntax error: unclosed quote\n");
 		return (NULL);
 	}
 	token = extract_token(s, end - s);
