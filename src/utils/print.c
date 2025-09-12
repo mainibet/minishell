c1d04860fdd5a3dad4b07cbcee66a382f33bce30
mainibet <mainibet@gmail.com>
2025-09-12 08:27:12 +0200

    New functino print warn ctrl_d with wwrite

--- src/utils/print.c
+++ src/utils/print.c
@@ -6,11 +6,22 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 07:40:52 by albetanc          #+#    #+#             */
-/*   Updated: 2025/09/12 07:46:33 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/09/12 08:25:59 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	print_warn_ctrl_d(int line_num, const char *delimiter)
+{
+	write(STDERR_FILENO,
+		"minishell: warning: here-document delimited by end-of-file ",
+		66);
+	ft_putnbr_fd_un((unsigned long)line_num, STDERR_FILENO);
+	write(STDERR_FILENO, " (wanted `", 10);
+	write(STDERR_FILENO, delimiter, ft_strlen(delimiter));
+	write(STDERR_FILENO, "')\n", 3);
+}
 
 void	ft_print_syntax_error(const char *token)
 {
