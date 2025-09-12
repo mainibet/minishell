a169cece28b6c972f1a27289fa7313ca0888b332
mainibet <mainibet@gmail.com>
2025-09-12 16:53:59 +0200

    commented alld files parser_redir.c it looks is not being used

--- src/parser/parser_redir.c
+++ src/parser/parser_redir.c
@@ -6,62 +6,64 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 14:31:58 by albetanc          #+#    #+#             */
-/*   Updated: 2025/09/12 16:41:11 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/09/12 16:52:44 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	**copy_token_strings(t_token *cmd_tokens, char **argv)
-{
-	t_token	*current;
-	int		i;
+// char	**copy_token_strings(t_token *cmd_tokens, char **argv)
+// {
+// 	t_token	*current;
+// 	int		i;
 
-	i = 0;
-	current = cmd_tokens;
-	while (current)
-	{
-		argv[i] = ft_strdup(current->txt);
-		if (!argv[i])
-		{
-			free_array(argv);
-			return (NULL);
-		}
-		i++;
-		current = current->next;
-	}
-	argv[i] = NULL;
-	return (argv);
-}
+// 	i = 0;
+// 	current = cmd_tokens;
+// 	while (current)
+// 	{
+// 		argv[i] = ft_strdup(current->txt);
+// 		if (!argv[i])
+// 		{
+// 			free_array(argv);
+// 			return (NULL);
+// 		}
+// 		i++;
+// 		current = current->next;
+// 	}
+// 	argv[i] = NULL;
+// 	return (argv);
+// }
 
-static int	ft_lstsize(t_token *lst)
-{
-	int	count;
+// static int	ft_lstsize(t_token *lst)
+// {
+// 	int	count;
 
-	count = 0;
-	while (lst)
-	{
-		count++;
-		lst = lst->next;
-	}
-	return (count);
-}
+// 	count = 0;
+// 	while (lst)
+// 	{
+// 		count++;
+// 		lst = lst->next;
+// 	}
+// 	return (count);
+// }
 
-char	**build_argv_from_tokens(t_token *cmd_tokens)
-{
-	char	**argv;
+// char	**build_argv_from_tokens(t_program *program, t_token *cmd_tokens)
+// {
+// 	char	**argv;
 
-	argv = malloc(sizeof(char *) * (ft_lstsize(cmd_tokens) + 1));
-	if (!argv)
-	{
-		free_token(cmd_tokens);
-		return (NULL);
-	}
-	argv = copy_token_strings(cmd_tokens, argv);
-	if (!argv)
-	{
-		free_token(cmd_tokens);
-		return (NULL);
-	}
-	return (argv);
-}
+// 	argv = malloc(sizeof(char *) * (ft_lstsize(cmd_tokens) + 1));
+// 	if (!argv)
+// 	{
+// 		free_token(cmd_tokens);
+// 		program->last_exit_status = 1;
+// 		return (NULL);
+// 	}
+// 	argv = copy_token_strings(cmd_tokens, argv);
+// 	if (!argv)
+// 	{
+// 		free_token(cmd_tokens);
+// 		program->last_exit_status = 1;
+// 		return (NULL);
+// 	}
+// 	return (argv);
+// }
